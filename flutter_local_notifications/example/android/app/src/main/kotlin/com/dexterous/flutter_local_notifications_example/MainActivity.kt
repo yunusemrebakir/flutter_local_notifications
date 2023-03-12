05e3e3af2c1340fce751353f42c49b0d837bbc72
Yunus Emre Bakir <yunusbakir92@gmail.com>
2023-03-12 18:12:12 +0100

    more cleanup

--- flutter_local_notifications/example/android/app/src/main/kotlin/com/dexterous/flutter_local_notifications_example/MainActivity.kt
+++ flutter_local_notifications/example/android/app/src/main/kotlin/com/dexterous/flutter_local_notifications_example/MainActivity.kt
@@ -12,23 +12,5 @@ import java.util.*
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "dexterx.dev/flutter_local_notifications_example").setMethodCallHandler { call, result ->
-            if ("drawableToUri" == call.method) {
-                val resourceId = this@MainActivity.resources.getIdentifier(call.arguments as String, "drawable", this@MainActivity.packageName)
-                result.success(resourceToUriString(this@MainActivity.applicationContext, resourceId))
-            }
-            if ("getAlarmUri" == call.method) {
-                result.success(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM).toString())
-            }
-        }
-    }
-
-    private fun resourceToUriString(context: Context, resId: Int): String? {
-        return (ContentResolver.SCHEME_ANDROID_RESOURCE + "://"
-                + context.resources.getResourcePackageName(resId)
-                + "/"
-                + context.resources.getResourceTypeName(resId)
-                + "/"
-                + context.resources.getResourceEntryName(resId))
     }
 }
